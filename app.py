edaf51ee5cde7e95abe6fa2057e078a250540e81
Kaokong <khaokong@gmail.com>
2022-02-16 18:34:04 +0000

    init flask

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 #import Flask
 from flask import Flask, render_template
+
 #create an instance of Flask
 app = Flask(__name__)
+#Route to home page
 @app.route('/')
 def home():
     return render_template('home.html')
